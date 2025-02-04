36a1c868877b3f584a7047fb20a13b77c589d81e
Martin Moradi <moradi.martin@gmail.com>
2025-02-05 00:03:20 +0100

    chore(docker): set up containerized development and production environments

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import path from 'node:path';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  reactStrictMode: true,
+  output: 'standalone',
+  outputFileTracingRoot: path.join(__dirname, '../../'),
 };
-
-export default nextConfig;
