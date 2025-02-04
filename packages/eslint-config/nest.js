50b9f798df2ac0c7021366342f73a06918a7dce7
Martin Moradi <moradi.martin@gmail.com>
2025-02-04 16:12:05 +0100

    chore(eslint): base eslint configuration

---
+++ packages/eslint-config/nest.js
@@ -0,0 +1,38 @@
+import { config as baseConfig } from './base.js';
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/**
+ * A custom ESLint configuration for NestJS applications.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ */
+export const nestConfig = [
+  ...baseConfig,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'module',
+      parserOptions: {
+        project: true,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+    },
+  },
+];
