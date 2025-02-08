871611767ef97732cca418d1d982e866a18081b8
Martin Moradi <moradi.martin@gmail.com>
2025-02-08 20:12:16 +0100

    chore(api): extend vitest config

--- packages/vitest-config/nest.js
+++ packages/vitest-config/nest.js
@@ -1,3 +1,25 @@
+import { defineConfig } from 'vitest/config';
 import { baseConfig } from './base.js';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default baseConfig;
+export default defineConfig({
+  ...baseConfig,
+  plugins: [...(baseConfig.plugins || []), tsconfigPaths()],
+  test: {
+    ...baseConfig.test,
+    environment: 'node',
+    globals: true,
+    include: ['**/*.spec.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.ts'],
+      exclude: [
+        'src/**/*.spec.ts',
+        'src/**/*.test.ts',
+        'src/**/*.module.ts',
+        'src/main.ts',
+      ],
+    },
+  },
+});
