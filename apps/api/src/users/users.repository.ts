bfd7707bbac8de0d4fcae62e23d8e75bf78e55ed
Martin Moradi <moradi.martin@gmail.com>
2025-02-08 20:13:17 +0100

    refactor(api): astract drizzle implementation

--- apps/api/src/users/users.repository.ts
+++ apps/api/src/users/users.repository.ts
@@ -1,19 +1,16 @@
 import { BaseRepository } from '@/database/base.repository';
 import { Injectable } from '@nestjs/common';
-import type { Schema } from '@repo/database';
-import { InferSelectModel } from 'drizzle-orm';
+import { queries } from '@repo/database';
+import { User } from '@repo/types';
 
 @Injectable()
 export class UsersRepository extends BaseRepository {
-  async findAll(): Promise<InferSelectModel<Schema['users']>[]> {
-    return await this.db.query.users.findMany({
-      columns: {
-        id: true,
-        name: true,
-        email: true,
-        createdAt: true,
-        updatedAt: true,
-      },
-    });
+  async findAll(): Promise<User[] | null> {
+    const users = await this.db.query.users.findMany();
+    return users?.length ? users : null;
+  }
+  async findById(id: string): Promise<User | null> {
+    const user = await this.db.query.users.findFirst(queries.byId(id));
+    return user ?? null;
   }
 }
