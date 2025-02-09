5141757f58ee0adefdc58258646df95a37dba37f
Martin Moradi <moradi.martin@gmail.com>
2025-02-09 22:34:03 +0100

    fix(api): fix imports app.module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,17 +1,15 @@
-import { DatabaseModule } from '@/database/database.module';
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
+import { DatabaseModule } from '@/database/database.module';
 import { UsersModule } from '@/users/users.module';
-import { AuthModule } from '@/auth/auth.module';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UsersModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
