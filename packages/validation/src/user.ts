119ee03fb25cc480f477ef8db331a2806ee9e934
Martin Moradi <moradi.martin@gmail.com>
2025-02-09 23:21:34 +0100

    feat(shared): implement zod createUserSchema

---
+++ packages/validation/src/user.ts
@@ -0,0 +1,22 @@
+import { UserInsert } from '@repo/types';
+import { z } from 'zod';
+
+export const createUserSchema: z.ZodType<UserInsert> = z.object({
+  email: z
+    .string()
+    .email('Invalid email address')
+    .min(1, 'Email is required')
+    .max(255, 'Email must be less than 255 characters long'),
+  name: z
+    .string()
+    .min(2, 'Name must be at least 2 characters long')
+    .max(100, 'Name must be less than 100 characters long'),
+  password: z
+    .string()
+    .min(8, 'Password must be at least 8 characters long')
+    .max(100, 'Password must be less than 100 characters long')
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/,
+      'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+    ),
+});
