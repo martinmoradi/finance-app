9f6c63acb5e68bf1fd8c31c4a32219995ac98903
Martin Moradi <moradi.martin@gmail.com>
2025-02-10 19:23:20 +0100

    feat(api): implement jwtconfig into auth.module

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,14 +1,21 @@
 import { AuthController } from '@/auth/auth.controller';
 import { AuthService } from '@/auth/auth.service';
+import jwtConfig from '@/config/jwt.config';
 import { UserRepository } from '@/user/user.repository';
 import { UserService } from '@/user/user.service';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 /**
- * Module for handling authentication-related functionality
- * Provides controllers and services for authentication operations
+ * Module for handling authentication-related functionality.
+ * Provides controllers and services for authentication operations.
  */
 @Module({
+  imports: [
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserService, UserRepository],
 })
