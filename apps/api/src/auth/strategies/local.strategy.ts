d79b33a2a30787a1a1f3fa58dd5cb4ea7ad446e6
Martin Moradi <moradi.martin@gmail.com>
2025-02-10 18:46:17 +0100

    feat(api): implement local auth strategy

---
+++ apps/api/src/auth/strategies/local.strategy.ts
@@ -0,0 +1,36 @@
+import { AuthService } from '@/auth/auth.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { User } from '@repo/types';
+import { Strategy } from 'passport-local';
+
+/**
+ * Local authentication strategy for Passport
+ * Validates user credentials against the database
+ */
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      usernameField: 'email',
+    });
+  }
+
+  /**
+   * Validates user credentials
+   * @param email - User's email
+   * @param password - User's password
+   * @returns User object with minimal details
+   * @throws UnauthorizedException if credentials are invalid
+   */
+  async validate(
+    email: string,
+    password: string,
+  ): Promise<Pick<User, 'id' | 'email' | 'name'>> {
+    const user = await this.authService.validateLocalUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
