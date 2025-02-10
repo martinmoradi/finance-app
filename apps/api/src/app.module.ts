b04ee6d7c72fd35f0538f3352214159d52baffc5
Martin Moradi <moradi.martin@gmail.com>
2025-02-10 01:55:51 +0100

    feat(api): implement global zod pipe validation

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,14 +4,18 @@ import { DatabaseModule } from '@/database/database.module';
 import { UserModule } from '@/user/user.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
+import { ZodValidationPipe } from 'nestjs-zod';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_PIPE, useClass: ZodValidationPipe }],
 })
 export class AppModule {}
