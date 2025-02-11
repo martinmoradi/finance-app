fd26711465643bd90abc47c865d5d6b257f009a1
Martin Moradi <moradi.martin@gmail.com>
2025-02-11 02:41:52 +0100

    refactor(api): use new user types in UserRepository

--- apps/api/src/user/user.repository.ts
+++ apps/api/src/user/user.repository.ts
@@ -2,7 +2,7 @@ import { BaseRepository } from '@/database/base.repository';
 import { CreateUserDto } from '@/user/dto/create-user.dto';
 import { Injectable } from '@nestjs/common';
 import { queries, users } from '@repo/database';
-import { User } from '@repo/types';
+import { DatabaseUser } from '@repo/types';
 
 /**
  * Repository for managing user data in the database.
@@ -14,7 +14,7 @@ export class UserRepository extends BaseRepository {
    * Retrieves all users from the database.
    * @returns Array of users if found, null if no users exist.
    */
-  async findAll(): Promise<User[] | null> {
+  async findAll(): Promise<DatabaseUser[] | null> {
     const users = await this.db.query.users.findMany();
     return users?.length ? users : null;
   }
@@ -24,7 +24,7 @@ export class UserRepository extends BaseRepository {
    * @param id - The user's unique identifier.
    * @returns User if found, null if not found.
    */
-  async findById(id: string): Promise<User | null> {
+  async findById(id: string): Promise<DatabaseUser | null> {
     const user = await this.db.query.users.findFirst(queries.byId(id));
     return user ?? null;
   }
@@ -34,7 +34,7 @@ export class UserRepository extends BaseRepository {
    * @param email - The user's email address.
    * @returns User if found, null if not found.
    */
-  async findByEmail(email: string): Promise<User | null> {
+  async findByEmail(email: string): Promise<DatabaseUser | null> {
     const user = await this.db.query.users.findFirst(queries.byEmail(email));
     return user ?? null;
   }
@@ -44,7 +44,7 @@ export class UserRepository extends BaseRepository {
    * @param createUserDto - Data transfer object containing user creation data.
    * @returns Newly created user if successful, null if creation fails.
    */
-  async create(createUserDto: CreateUserDto): Promise<User | null> {
+  async create(createUserDto: CreateUserDto): Promise<DatabaseUser | null> {
     const newUser = await this.db
       .insert(users)
       .values(createUserDto)
