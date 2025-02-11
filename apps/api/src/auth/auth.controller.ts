3374518593da92ba2d6092744972502af831654e
Martin Moradi <moradi.martin@gmail.com>
2025-02-11 02:42:48 +0100

    refactor(api): use new user types in AuthController

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,50 +1,41 @@
 import { AuthService } from '@/auth/auth.service';
-import { LocalAuthGuard } from '@/auth/guards/local-auth/local-auth.guard';
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from '@/auth/guards/local-auth.guard';
 import { CreateUserDto } from '@/user/dto/create-user.dto';
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthUser } from '@repo/types';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthenticatedUser, PublicUser } from '@repo/types';
 import { Request as ExpressRequest } from 'express';
-import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 
-/**
- * Controller handling authentication-related endpoints.
- * Provides endpoints for user registration and authentication.
- */
-@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  @ApiOperation({ summary: 'Register a new user' })
-  @ApiBody({ type: CreateUserDto })
-  @ApiResponse({
-    status: 201,
-    description: 'User successfully registered',
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Invalid input data',
-  })
-  signup(@Body() createUserDto: CreateUserDto) {
+  signup(@Body() createUserDto: CreateUserDto): Promise<AuthenticatedUser> {
     return this.authService.signup(createUserDto);
   }
 
   @UseGuards(LocalAuthGuard)
   @Post('signin')
-  @ApiOperation({ summary: 'Authenticate existing user' })
-  @ApiResponse({
-    status: 200,
-    description: 'User successfully authenticated',
-  })
-  @ApiResponse({
-    status: 401,
-    description: 'Unauthorized - Invalid credentials',
-  })
   signin(
-    @Request()
-    req: ExpressRequest & { user: AuthUser },
-  ) {
+    @Request() req: ExpressRequest & { user: PublicUser },
+  ): Promise<AuthenticatedUser> {
     return this.authService.signin(req.user);
+  }
+
+  // Dummy route to test the JWT guard
+  @UseGuards(JwtAuthGuard) // This guard checks the JWT token
+  @Get('me')
+  me(
+    @Request() req: ExpressRequest & { user: AuthenticatedUser },
+  ): AuthenticatedUser {
+    return req.user;
   }
 }
