d1f24c706e0e4a972bbd28bd5b8bc613aa337a31
Martin Moradi <moradi.martin@gmail.com>
2025-02-11 13:30:33 +0100

    feat(api): implement more robust types jwt config

--- apps/api/src/config/jwt.config.ts
+++ apps/api/src/config/jwt.config.ts
@@ -1,11 +1,16 @@
 import { getRequiredEnvVar } from '@repo/env-validation';
 import { registerAs } from '@nestjs/config';
+import { JwtModuleOptions } from '@nestjs/jwt';
+
+// Make secret required
+type RequiredJwtModuleOptions = Required<Pick<JwtModuleOptions, 'secret'>> &
+  Omit<JwtModuleOptions, 'secret'>;
 
 /**
  * Configuration for JWT settings.
  * Provides the secret key and expiration time for JWT tokens
  */
-export default registerAs('jwt', () => {
+export default registerAs('jwt', (): RequiredJwtModuleOptions => {
   const secret = getRequiredEnvVar('JWT_SECRET');
   const expiresIn = getRequiredEnvVar('JWT_EXPIRES_IN');
 
