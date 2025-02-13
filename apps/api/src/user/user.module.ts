68cca38899c229f5f31bf8c1ec102a03dda85340
Martin Moradi <moradi.martin@gmail.com>
2025-02-13 22:11:27 +0100

    fix(api): add missing imports in UserModule

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -1,15 +1,17 @@
-import { DatabaseService } from '@/database/database.service';
-import { Module } from '@nestjs/common';
+import { DatabaseModule } from '@/database/database.module';
+import { LoggerModule } from '@/logger/logger.module';
 import { UserController } from '@/user/user.controller';
 import { UserRepository } from '@/user/user.repository';
 import { UserService } from '@/user/user.service';
-
+import { Module } from '@nestjs/common';
 /**
  * Module for managing user-related functionality.
  * Provides controllers and services for user operations.
  */
 @Module({
+  imports: [DatabaseModule, LoggerModule],
   controllers: [UserController],
-  providers: [UserRepository, UserService, DatabaseService],
+  providers: [UserRepository, UserService],
+  exports: [UserService],
 })
 export class UserModule {}
