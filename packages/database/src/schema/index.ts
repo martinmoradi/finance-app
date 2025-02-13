ea546eff86d9b4882995acdbdff41a69feaffe83
Martin Moradi <moradi.martin@gmail.com>
2025-02-13 22:00:17 +0100

    fix(db): fix schema exports from @repo/database

--- packages/database/src/schema/index.ts
+++ packages/database/src/schema/index.ts
@@ -1,8 +1,12 @@
+import { sessions } from './sessions';
 import { users } from './users';
+
 export * from './users';
+export * from './sessions';
 
 export const schema = {
   users,
+  sessions,
 } as const;
 
 export type Schema = typeof schema;
