c873533349833b8a01f9a5a9cdb4c17f8edcf09d
Martin Moradi <moradi.martin@gmail.com>
2025-02-13 22:12:58 +0100

    feat(api): implement @nestjs/schedule for cronjobs

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,8 +5,11 @@ import { UserModule } from '@/user/user.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
+import { ScheduleModule } from '@nestjs/schedule';
 import { ZodValidationPipe } from 'nestjs-zod';
 import { AuthModule } from './auth/auth.module';
+import { LoggerModule } from './logger/logger.module';
+import { SessionModule } from './session/session.module';
 
 /**
  * Main application module.
@@ -18,6 +21,9 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UserModule,
+    ScheduleModule.forRoot(),
+    SessionModule,
+    LoggerModule,
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_PIPE, useClass: ZodValidationPipe }],
