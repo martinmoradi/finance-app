9e8d6c19b3c1a5a2f33a06d883148642c0ecd173
Martin Moradi <moradi.martin@gmail.com>
2025-02-14 04:11:44 +0100

    fix(db): fix relationships schema export

--- packages/database/src/schema/index.ts
+++ packages/database/src/schema/index.ts
@@ -1,12 +1,19 @@
-import { sessions } from './sessions';
-import { users } from './users';
+import { sessions, sessionsRelations } from './sessions';
+import { users, usersRelations } from './users';
 
-export * from './users';
 export * from './sessions';
+export * from './users';
+
+export const tables = {
+  users,
+  sessions,
+} as const;
 
 export const schema = {
   users,
+  usersRelations,
   sessions,
+  sessionsRelations,
 } as const;
 
 export type Schema = typeof schema;
