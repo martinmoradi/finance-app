97458a2c210690add87bd060c8c6a67446e33cb9
Martin Moradi <moradi.martin@gmail.com>
2025-02-14 04:10:35 +0100

    feat(db): add sql email check and auto updated at

--- packages/database/src/schema/users.ts
+++ packages/database/src/schema/users.ts
@@ -1,16 +1,33 @@
 import { sessions } from '@/schema/sessions';
-import { eq, InferInsertModel, InferSelectModel, relations } from 'drizzle-orm';
-import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import {
+  eq,
+  InferInsertModel,
+  InferSelectModel,
+  relations,
+  sql,
+} from 'drizzle-orm';
+import { check, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 
 // Table definition
-export const users = pgTable('users', {
-  id: uuid('id').primaryKey().defaultRandom(),
-  name: text('name').notNull(),
-  email: text('email').notNull().unique(),
-  password: text('password').notNull(),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').defaultNow().notNull(),
-});
+export const users = pgTable(
+  'users',
+  {
+    id: uuid('id').primaryKey().defaultRandom(),
+    name: text('name').notNull(),
+    email: text('email').notNull().unique(),
+    password: text('password').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at')
+      .defaultNow()
+      .$onUpdate(() => new Date()),
+  },
+  (t) => [
+    check(
+      'valid_email',
+      sql`${t.email} ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'`,
+    ),
+  ],
+);
 
 // Relations;
 export const usersRelations = relations(users, ({ many }) => ({
