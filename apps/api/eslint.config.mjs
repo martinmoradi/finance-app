14cf757d6f316b857b329d379593b07fe96ec4b3
Martin Moradi <moradi.martin@gmail.com>
2025-02-21 17:30:22 +0100

    chore(api): simplify eslint config

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -2,12 +2,6 @@
 import { nestConfig } from '@repo/eslint-config/nest';
 
 /** @type {import("eslint").Linter.Config[]} */
-const config = [
-  ...nestConfig,
-  {
-    // Add any API-specific rules here
-    rules: {},
-  },
-];
+const config = nestConfig;
 
 export default config;
