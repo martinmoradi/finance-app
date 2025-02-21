663a17887c41801fcd043714da3ced533ca18cd0
Martin Moradi <moradi.martin@gmail.com>
2025-02-21 17:41:55 +0100

    chore(db): setup e2e tests

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -2,5 +2,5 @@ export * from 'drizzle-orm';
 export * from './client';
 export * from './schema';
 export * from './seed';
-export * from './tests/helpers/db';
+export * from './e2e/helpers/db';
 export * as pg from 'pg';
