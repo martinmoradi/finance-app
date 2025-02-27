ec7da296072c88ba9d6d7487e32c32a7b317036f
Martin Moradi <moradi.martin@gmail.com>
2025-02-27 17:25:36 +0100

    chore(api): jest coverage config

--- apps/api/jest-e2e.config.ts
+++ apps/api/jest-e2e.config.ts
@@ -27,13 +27,5 @@ module.exports = {
   ],
   coverageDirectory: './coverage',
   coverageReporters: ['json', 'lcov', 'text', 'html'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
   forceCoverageMatch: ['**/*.ts'],
 };
