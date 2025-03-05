2b578feccb730be361081407fcc82f14a6105277
Martin Moradi <moradi.martin@gmail.com>
2025-03-05 23:31:20 +0100

    feat(web): expend getSession server action

--- apps/web/src/lib/api/auth/get-session.ts
+++ apps/web/src/lib/api/auth/get-session.ts
@@ -3,13 +3,41 @@
 import { PublicUser } from '@repo/types';
 import { cookies } from 'next/headers';
 
-export async function getSession(): Promise<PublicUser | null> {
+type SessionData = {
+  data: PublicUser;
+  expiresAt: number;
+};
+
+type Session =
+  | {
+      isAuthenticated: true;
+      user: PublicUser;
+      isExpired: boolean;
+      expiresSoon: boolean;
+    }
+  | {
+      isAuthenticated: false;
+    };
+
+/**
+ * Server action helper to get the current user's session
+ */
+export async function getSession(): Promise<Session> {
   const cookieStore = await cookies();
   const session = cookieStore.get('session');
 
   if (!session) {
-    return null;
+    return { isAuthenticated: false };
   }
 
-  return JSON.parse(session.value);
+  const sessionData = JSON.parse(session.value) as SessionData;
+  const isExpired = Date.now() > sessionData.expiresAt;
+  const expiresSoon = sessionData.expiresAt - Date.now() < 5 * 60 * 1000; // 5 minutes
+
+  return {
+    isAuthenticated: true,
+    user: sessionData.data,
+    isExpired,
+    expiresSoon,
+  };
 }
