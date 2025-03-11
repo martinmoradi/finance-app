62d794d3f385f7f978af122d69684b3846673adc
Martin Moradi <moradi.martin@gmail.com>
2025-03-11 18:57:32 +0100

    refactor(web): remove main from app layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${publicSans.variable} antialiased`}>
-        <main>{children}</main>
+        {children}
         <Toaster />
       </body>
     </html>
