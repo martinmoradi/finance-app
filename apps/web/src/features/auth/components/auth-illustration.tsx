2283b46cf1af5f6b84e6d173ad6ba0395176a1a1
Martin Moradi <moradi.martin@gmail.com>
2025-03-11 19:00:52 +0100

    feat(web): implement authIllustration component

---
+++ apps/web/src/features/auth/components/auth-illustration.tsx
@@ -0,0 +1,34 @@
+import { Display, Body } from '@/components/ui/typography';
+import LogoLarge from '@public/logo-large.svg';
+import IllustrationAuthentication from '@public/illustration-authentication.svg';
+
+export function AuthIllustration() {
+  return (
+    <div className='relative'>
+      {/* Base illustration without text */}
+      <IllustrationAuthentication className='w-full h-full object-fit rounded-xl' />
+
+      {/* Logo overlay */}
+      <div className='absolute top-10 left-10'>
+        <LogoLarge className='h-10' />
+      </div>
+
+      {/* Text content overlay */}
+      <div className='absolute left-10 max-w-[48rem] top-[80%]'>
+        <div className='mb-6'>
+          <Display>
+            <span className='text-white'>
+              Keep track of your money and save for your future
+            </span>
+          </Display>
+        </div>
+        <Body>
+          <span className='text-white'>
+            Personal finance app puts you in control of your spending. Track
+            transactions, set budgets, and add to savings pots easily.
+          </span>
+        </Body>
+      </div>
+    </div>
+  );
+}
