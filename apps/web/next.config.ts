429fe7d37e76744656cd39ccdd5d7a9f4202caba
Martin Moradi <moradi.martin@gmail.com>
2025-03-16 01:16:04 +0100

    chore(web): install next-intl

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,13 +1,27 @@
-import path from 'node:path';
 import { withSentryConfig } from '@sentry/nextjs';
-const nextConfig = {
+import { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+import path from 'node:path';
+import type { Configuration as WebpackConfig, RuleSetRule } from 'webpack';
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   outputFileTracingRoot: path.join(__dirname, '../../'),
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  webpack(config: any) {
-    config.module.rules.push({
+  webpack(config: WebpackConfig): WebpackConfig {
+    const rules = config.module?.rules as RuleSetRule[];
+    rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
@@ -15,31 +29,17 @@ const nextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+const withNextIntl = createNextIntlPlugin();
+
+export default withSentryConfig(withNextIntl(nextConfig), {
   org: 'martin-moradi',
   project: 'finance-next',
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
   reactComponentAnnotation: {
     enabled: true,
   },
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
   tunnelRoute: '/monitoring',
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
   automaticVercelMonitors: true,
 });
