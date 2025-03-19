35d5029964ee9201d338129b4d1efcbc0154b1b8
Martin Moradi <moradi.martin@gmail.com>
2025-03-19 21:42:18 +0100

    chore(web): turn off no require imports for test files

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -55,6 +55,7 @@ const eslintConfig = [
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
     },
   },
 ];
